6ed7e2b5f2ced38d81ea07aab35ed11b323a32c8
Shah-ikbal <shahikbalahmed@gmail.com>
2025-01-29 10:56:38 +0530

    site url updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Shah Ikbal Ahmed',
     description:
       'Shah Ikbal Ahmed is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: '', // No trailing slash allowed!
+    siteUrl: 'https://shahikbal.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '',
   },
